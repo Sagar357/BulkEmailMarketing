77030ab4eb9ab6512a70d07eca1f6bd957bfba30
admin <admin@ADMIN-PC>
2020-11-12 11:01:12 +0530

    commit

---
+++ Scripts/main.js
@@ -0,0 +1,67 @@
+﻿var fnclose = event => {
+    var x = event.target.dataset.target;
+    var elem = document.getElementsByClassName(x);
+    elem[0].style.display = "none";
+    if (event.target.dataset.link != undefined && event.target.dataset.link != '') {
+        //     alert(event.target.dataset.link) 
+        var Campaign_wrp = document.getElementsByClassName('Campaign_wrp');
+        for (i = 0; i < Campaign_wrp.length; i++) {
+            if (Campaign_wrp[i].attributes.id != undefined) {
+                var selector = Campaign_wrp[i].attributes.id.value;
+                document.getElementById(selector).style.display = 'block';
+            }
+        }
+    }
+    //     debugger
+}
+
+function FormSubmit() {
+    var x = document.querySelectorAll(".FormPop");
+    if (x[0].style.display === "block") {
+        x[0].style.display = "none";
+    } else {
+        x[0].style.display = "block";
+    }
+}
+
+// end first function
+
+function FormSubmit2() {
+    var y = document.getElementsByClassName("FormPop");
+    var x = document.getElementsByClassName("FormPop2");
+    if (x[0].style.display === "block") {
+        x[0].style.display = "none";
+    } else {
+        x[0].style.display = "block";
+        y[0].style.display = "none";
+    }
+}
+
+
+// prodcut//
+
+function openProduct(productName) {
+    var i;
+    var x = document.getElementsByClassName("product");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(productName).style.display = "block";
+}
+
+
+//chekbox
+//Jquery code start
+
+$('#checkbox').change(function () {
+    // if()
+    $("form input:checkbox").prop('checked', 'true');
+
+});
+
+
+// flip
+
+$(".Camp_chain").click(function () {
+    $(".L2").slideToggle(500).css("display-block");
+}); 
