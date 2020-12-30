65db679849b682fef230e1b5b097ae9f54dc333f
@Sagar357 <markushno357@gmail.com>
2020-12-30 14:27:57 +0530

    commit

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -118,10 +118,11 @@ namespace BulkEmailMarketing.Services
                     //client.Host = "relay-hosting.secureserver.net";
                     //client.Host = "host.earthithub.com";
                     client.Host = smtpDetail.serverString;
-                    //client.Port = 25;
-                    client.Port = 587;
+                    client.Port = 25;
+                   // client.Port = 587;
                     client.UseDefaultCredentials = false;
-                    client.EnableSsl = true;
+                    //client.EnableSsl = true;
+                    client.EnableSsl = false;
                     client.Credentials = new System.Net.NetworkCredential(message.Sender.Address.ToString(), smtpDetail.password.ToString());
 
                     /*Email Blaster*/
